d7aa97f012ee5d8f300ac6cda161725c0101ae19
Abhay Asodariya <121076012+AbhayAsodariya@users.noreply.github.com>
2024-10-04 18:31:44 +0530

    Update productRoutes.js

--- routes/Product/productRoutes.js
+++ routes/Product/productRoutes.js
@@ -22,7 +22,7 @@ router.post("/profile/:id", jwtAuthMiddleware, createAddToCart);
 router.post("/", createGlobleProduct);
 router.get("/", getGlobalProducts);
 router.get("/:id", getProductById);
-router.post("/:id",  editProduct);
-router.delete("/:id", deleteProduct);
+router.post("/edit/:id",  editProduct);
+router.delete("/delete/:id", deleteProduct);
 
 module.exports = router;
