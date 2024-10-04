79f705365be1be9aa1943198414fc26ca4c24ff6
9106458583 <104250550+SAHIL07PATEL@users.noreply.github.com>
2024-10-04 13:32:45 +0530

    .

--- routes/Product/productRoutes.js
+++ routes/Product/productRoutes.js
@@ -14,7 +14,7 @@ const router = express.Router();
 router.post("/profile/:id", jwtAuthMiddleware, createAddToCart);
 router.post("/", createGlobleProduct);
 router.get("/", getGlobalProducts);
-router.get("/:id",jwtAuthMiddleware, getProductById); // Get product by ID
+router.get("/:id", getProductById); // Get product by ID
 router.post("/:id", jwtAuthMiddleware, editProduct); // Edit product using POST
 router.delete("/:id", jwtAuthMiddleware, deleteProduct); // Delete product
 
